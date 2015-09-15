feaa01d7aa081227b62fad435ac671158fdf8448
itai-codefresh <itai@codefresh.io>
2015-09-15 22:19:41 +0300

    Update app.js
    rename default page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static('./'));
 
 app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/Welcom.html');
+    res.sendFile(__dirname + '/Welcome.html');
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
